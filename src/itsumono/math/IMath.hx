bdb592000b160b1abc1a7d33a2fd70852965a7dc
Antoine Gersant <antoine.gersant@lesforges.org>
2014-02-03 20:15:08 -0800

    Added utils class for integer math

---
+++ src/itsumono/math/IMath.hx
@@ -0,0 +1,17 @@
+package itsumono.math;
+
+/**
+ * @author agersant
+ */
+
+class IMath {
+
+	public static inline function max (a : Int, b : Int) : Int {
+		return a > b ? a : b;
+	}
+	
+	public static inline function min (a : Int, b : Int) : Int {
+		return a < b ? a : b;
+	}
+	
+}
