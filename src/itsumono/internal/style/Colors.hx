6dccd271c0cdafda8affb7c2d2ff07864ff62220
Antoine Gersant <antoine.gersant@lesforges.org>
2014-02-06 23:27:21 -0800

    Added palette for internal use

---
+++ src/itsumono/internal/style/Colors.hx
@@ -0,0 +1,20 @@
+package itsumono.internal.style;
+
+/**
+ * @author agersant
+ */
+
+class Colors {
+
+	// Greys
+	public static inline var black : Int			= 0x000000;
+	public static inline var grey30 : Int			= 0x4D4D4D;
+	public static inline var grey60 : Int			= 0x999999;
+	public static inline var white : Int			= 0xFFFFFF;
+	
+	// Itsumono
+	public static inline var primary : Int			= 0x001226;
+	public static inline var accent1 : Int			= 0x019fff;
+	public static inline var accent2 : Int			= 0xEB0D3C;
+	
+}
